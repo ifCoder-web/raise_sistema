054f4c68cd5e18e480971312ff34ec7172cc4568
ifCoder <ifcoderweb@gmail.com>
2021-04-27 20:51:36 -0300

    teste 2

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-	module.exports = {mongoURI: "mongodb+srv://ifcoderweb:Laranja-92@cluster0.lcvh8.mongodb.net/sistema1?retryWrites=true&w=majority"}
+	module.exports = {mongoURI: "mongodb+srv://ifcoderweb:92558164@cluster0.lcvh8.mongodb.net/sistema1?retryWrites=true&w=majority"}
 }else{
 	module.exports = {mongoURI: "mongodb://localhost:27017/sistema1"}
 }
