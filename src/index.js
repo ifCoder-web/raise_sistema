08488afd42211f37045c0c92cf1d4db6629e05d5
ifCoder <ifcoderweb@gmail.com>
2021-04-27 21:55:21 -0300

    pdf 2

--- src/index.js
+++ src/index.js
@@ -453,13 +453,13 @@ const db = require('./db/db.js')
    			});
 			// GERA O PDF
 				//let options = { format: 'A4', path: process.cwd() || "./meupdf.pdf" };
-				let options = { format: 'A4', name: "teste.pdf" };
+				let options = { format: 'A4', name: "teste.pdf", path: "./" };
 				let file = {content: conteudo};
 				await pdf.generatePdf(file, options).then(pdfBuffer => {
 				  	console.log("PDF Buffer:-", pdfBuffer);
 				  	req.flash("success_msg", "PDF gerado com sucesso!")
 				}).catch((err) => {
-					req.flash("error_msg", "Houve um erro ao gerar o arquivo!")
+					req.flash("error_msg", "Houve um erro ao gerar o arquivo!"+ err)
 					console.error('Houve um erro ao gerar o arquivo! '+ err)
 				});
 			// Redireciona para a pagina anterior	
