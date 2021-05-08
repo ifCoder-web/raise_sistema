100dc4b1bed567b7540c2c409de793841619be24
Igor Fraga <66173104+ifCoder-web@users.noreply.github.com>
2021-05-08 10:35:01 -0300

    Delete db.js

--- src/config/db.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV == "production"){
-	module.exports = {mongoURI: "mongodb+srv://ifcoderweb:92558164@cluster0.lcvh8.mongodb.net/sistema1?retryWrites=true&w=majority"}
-}else{
-	module.exports = {mongoURI: "mongodb://localhost:27017/sistema1"}
-}
